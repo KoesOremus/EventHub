9c5ed52cc4a1be5df74334c48bfa72b3b94f6925
KoesOremus <dibaburks@gmail.com>
2025-04-03 00:49:46 -0700

    Code

---
+++ app/src/main/java/com/example/event_hub/EventAdapter.java
@@ -0,0 +1,64 @@
+package com.example.event_hub;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventViewHolder> {
+
+    private List<Event> eventList;
+    private OnEventClickListener listener;
+
+    public interface OnEventClickListener {
+        void onEventClick(Event event);
+    }
+
+    public EventAdapter(List<Event> eventList, OnEventClickListener listener) {
+        this.eventList = eventList;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public EventViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_event, parent, false);
+        return new EventViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventViewHolder holder, int position) {
+        Event event = eventList.get(position);
+        holder.bind(event, listener);
+    }
+
+    @Override
+    public int getItemCount() {
+        return eventList.size();
+    }
+
+    static class EventViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        TextView titleTextView;
+        TextView locationTextView;
+
+        public EventViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.image_event);
+            titleTextView = itemView.findViewById(R.id.text_event_title);
+            locationTextView = itemView.findViewById(R.id.text_event_location);
+        }
+
+        public void bind(final Event event, final OnEventClickListener listener) {
+            titleTextView.setText(event.getTitle());
+            locationTextView.setText(event.getLocation());
+            imageView.setImageResource(event.getImageResource());
+
+            itemView.setOnClickListener(v -> listener.onEventClick(event));
+        }
+    }
+}
