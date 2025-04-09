a7cf7ca1e2d807489233a57d497701a58d5301ab
khushikishan <khushi.kishan2004@gmail.com>
2025-04-08 18:37:06 -0700

    secured api push part 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,6 +14,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val mapsApiKey: String = project.findProperty("MAPS_API_KEY") as? String ?: ""
+        manifestPlaceholders["mapsApiKey"] = mapsApiKey
     }
 
     buildFeatures {
