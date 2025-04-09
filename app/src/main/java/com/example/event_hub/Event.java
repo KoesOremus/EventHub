1a0566c31db2f059cacc7e163aa297d3c794b3cb
Nicholas-lkn <nlookkin@student.ubc.ca>
2025-04-09 13:37:28 -0700

    i might have messed up

--- app/src/main/java/com/example/event_hub/Event.java
+++ app/src/main/java/com/example/event_hub/Event.java
@@ -1,25 +1,105 @@
 package com.example.event_hub;
 
-public class Event {
-    private String title;
-    private String location;
-    private int imageResource;
+import android.os.Parcel;
+import android.os.Parcelable;
 
+public class Event implements Parcelable {
+    private int posterResourceId;
+    private int headerResourceId;
+    private String title;
+    private String description;
+    private String date;
+    private String location;
+    private double price;
+
+    // Full constructor
+    public Event(int posterResourceId, int headerResourceId, String title,
+                 String description, String date, String location, double price) {
+        this.posterResourceId = posterResourceId;
+        this.headerResourceId = headerResourceId;
+        this.title = title;
+        this.description = description;
+        this.date = date;
+        this.location = location;
+        this.price = price;
+    }
+
+    // Community events constructor
     public Event(String title, String location, int imageResource) {
         this.title = title;
         this.location = location;
-        this.imageResource = imageResource;
+        this.posterResourceId = imageResource;
+        this.headerResourceId = imageResource;
+        this.description = "";
+        this.date = "";
+        this.price = 0.0;
+    }
+
+    // Getters
+    public int getImageResource() {
+        return posterResourceId;
+    }
+
+    public int getHeaderResId() {
+        return headerResourceId;
     }
 
     public String getTitle() {
         return title;
     }
 
+    public String getDescription() {
+        return description;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
     public String getLocation() {
         return location;
     }
 
-    public int getImageResource() {
-        return imageResource;
+    public double getPrice() {
+        return price;
+    }
+
+
+    protected Event(Parcel in) {
+        posterResourceId = in.readInt();
+        headerResourceId = in.readInt();
+        title = in.readString();
+        description = in.readString();
+        date = in.readString();
+        location = in.readString();
+        price = in.readDouble();
+    }
+
+    public static final Creator<Event> CREATOR = new Creator<Event>() {
+        @Override
+        public Event createFromParcel(Parcel in) {
+            return new Event(in);
+        }
+
+        @Override
+        public Event[] newArray(int size) {
+            return new Event[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(posterResourceId);
+        dest.writeInt(headerResourceId);
+        dest.writeString(title);
+        dest.writeString(description);
+        dest.writeString(date);
+        dest.writeString(location);
+        dest.writeDouble(price);
     }
 }
--- app/src/main/java/com/example/event_hub/FeedbackFragment.java
+++ app/src/main/java/com/example/event_hub/FeedbackFragment.java
@@ -5,30 +5,71 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.RatingBar;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
 
 public class FeedbackFragment extends Fragment {
+
+    private Ticket ticket;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        if (getArguments() != null) {
+            ticket = getArguments().getParcelable("ticket");
+        }
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_feedback, container, false);
 
-        RatingBar ratingBar = view.findViewById(R.id.rating_bar);
-        Button submitButton = view.findViewById(R.id.button_submit);
 
-        submitButton.setOnClickListener(v -> {
-            // Handle rating submission
-            float rating = ratingBar.getRating();
-            // Save rating to database
+        if (ticket != null) {
+            TextView ticketDetailsTextView = view.findViewById(R.id.text_ticket_details);
 
-            // Return to event board
-            getParentFragmentManager().beginTransaction()
-                    .replace(R.id.fragment_container, new EventBoardFragment())
-                    .commit();
+            String ticketDetails = String.format(
+                    "Event: %s\n" +
+                            "Quantity: %d\n" +
+                            "Price: $%.2f\n" +
+                            "Customer: %s\n" +
+                            "Billing Address: %s",
+                    ticket.getEventName(),
+                    ticket.getQuantity(),
+                    ticket.getPrice(),
+                    ticket.getCustomerName(),
+                    ticket.getBillingAddress()
+            );
+
+            ticketDetailsTextView.setText(ticketDetails);
+
+        Event event = EventRepository.getEventByTitle(ticket.getEventName());
+        if (event != null) {
+            TextView eventFeedback = view.findViewById(R.id.text_event_details);
+            ImageView eventHeader = view.findViewById(R.id.feedbackImage);
+
+            eventFeedback.setText(event.getTitle()+"\n"+event.getDate()+"\n"+event.getLocation());
+            eventHeader.setImageResource(event.getImageResource());
+            }
+        }
+        Button submitFeedback = view.findViewById(R.id.button_submit_feedback);
+        submitFeedback.setOnClickListener(v->{
+            Toast.makeText(getActivity(), "Feedback and checkout Confirmed", Toast.LENGTH_SHORT).show();
+
+            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+            EventBoardFragment eventBoardFragment = new EventBoardFragment();
+            transaction.replace(R.id.fragment_container, eventBoardFragment);
+            transaction.addToBackStack(null);
+            transaction.commit();
         });
 
         return view;
